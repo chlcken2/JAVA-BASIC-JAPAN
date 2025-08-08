b16cf54d7d44f8315f6b716da87edf721063d0c2
창현 <changhyeon@changhyeon-ui-MacBookPro.local>
2025-08-08 10:46:00 +0900

    Initial commit

---
+++ src/test1/basic10.java
@@ -0,0 +1,17 @@
+package test1;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class basic10 {
+    public static void main(String[] arg) {
+        int bno, kor, eng, math, total;
+        String name;
+        double avg;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("bnt: " );
+        bno = sc.nextInt();
+    }
+}
